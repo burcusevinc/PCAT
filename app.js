b102d57c7afdf8ca247a6feb77813b0ec6e63cae
burcusevinc <sburcusevinc@gmail.com>
2023-02-13 14:50:18 +0300

    static files uploaded

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+
+const myLoggler = (req,res,next) => {
+  //request middleware: (req)
+  console.log("Middleware log 1")
+  //bir sonraki middleware'e ilerlemesi için yazılır
+  //yani response alır. (res)
+  next()
+}
+
+//MIDDLEWARE
+//express.static -> middleware function
+//public -> static dosyalar
+app.use(express.static('public'))
+app.use(myLoggler)
+
+// app.get('/', (req,res) => {
+//     const photo = {
+//         id: 1,
+//         name: "Photo Name",
+//         description: "Photo description"
+//       }
+//       res.send(photo)
+// })
+
 app.get('/', (req,res) => {
-    const photo = {
-        id: 1,
-        name: "Photo Name",
-        description: "Photo description"
-      }
-      res.send(photo)
+  //path modulu kullanıldı
+  //resolve:dosya yolunu çözmesi için
+  res.sendFile(path.resolve(__dirname,'temp/index.html'))
 })
 
 const port = 3000;

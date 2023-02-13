41a7932052d384890e968f2b9745602078879984
burcusevinc <sburcusevinc@gmail.com>
2023-02-13 15:37:47 +0300

    ejs and partials uploaded

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
+
+//TEMPLATE ENGINE
+//template engine olarak "ejs" kullanılacak.
+//views içindeki .html uzantılı dosyalar .ejs uzantılı yapılır
+app.set("view engine","ejs")
 
 
 const myLoggler = (req,res,next) => {
@@ -30,7 +36,16 @@ app.use(myLoggler)
 app.get('/', (req,res) => {
   //path modulu kullanıldı
   //resolve:dosya yolunu çözmesi için
-  res.sendFile(path.resolve(__dirname,'temp/index.html'))
+  //res.sendFile(path.resolve(__dirname,'temp/index.html'))
+
+  res.render('index')
+})
+
+app.get('/about', (req,res) => {
+  res.render('about')
+})
+app.get('/add', (req,res) => {
+  res.render('add')
 })
 
 const port = 3000;
